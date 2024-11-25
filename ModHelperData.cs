ce8a76b7a7bb1d9a6fe0a7bf141e4b7ae2acd841
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2024-11-25 11:03:56 -0500

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+namespace OrzathTheRealityWarper;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "45.0";
+    public const string Version = "1.1.0";
+    public const string Name = "Ozrath the Reality Warper";
+
+    public const string Description = "Adds the Monkey Hero Ozrath the Reality Warper";
+    public const string RepoOwner = "DatJaneDoe";
+    public const string RepoName = "OzrathTheRealityWarper";
+
+	
+}
